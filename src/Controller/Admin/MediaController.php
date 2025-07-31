296739d00bba2dbfc6dfd865b70107dc4a186198
Neeemos <enzo.belfi@galaxy-hotels.fr>
2025-07-31 15:49:35 +0200

    Update 5.4 to 6.4.24

--- src/Controller/Admin/MediaController.php
+++ src/Controller/Admin/MediaController.php
@@ -4,44 +4,42 @@ namespace App\Controller\Admin;
 
 use App\Entity\Media;
 use App\Form\MediaType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MediaController extends AbstractController
 {
-    /**
-     * @Route("/admin/media", name="admin_media_index")
-     */
-    public function index(Request $request)
+    #[Route('/admin/media', name: 'admin_media_index')]
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
         $page = $request->query->getInt('page', 1);
-
         $criteria = [];
 
         if (!$this->isGranted('ROLE_ADMIN')) {
             $criteria['user'] = $this->getUser();
         }
 
-        $medias = $this->getDoctrine()->getRepository(Media::class)->findBy(
+        $mediaRepository = $doctrine->getRepository(Media::class);
+        $medias = $mediaRepository->findBy(
             $criteria,
             ['id' => 'ASC'],
             25,
             25 * ($page - 1)
         );
-        $total = $this->getDoctrine()->getRepository(Media::class)->count([]);
+        $total = $mediaRepository->count([]);
 
         return $this->render('admin/media/index.html.twig', [
             'medias' => $medias,
             'total' => $total,
-            'page' => $page
+            'page' => $page,
         ]);
     }
 
-    /**
-     * @Route("/admin/media/add", name="admin_media_add")
-     */
-    public function add(Request $request)
+    #[Route('/admin/media/add', name: 'admin_media_add')]
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
         $media = new Media();
         $form = $this->createForm(MediaType::class, $media, ['is_admin' => $this->isGranted('ROLE_ADMIN')]);
@@ -51,26 +49,40 @@ class MediaController extends AbstractController
             if (!$this->isGranted('ROLE_ADMIN')) {
                 $media->setUser($this->getUser());
             }
-            $media->setPath('uploads/' . md5(uniqid()) . '.' . $media->getFile()->guessExtension());
-            $media->getFile()->move('uploads/', $media->getPath());
-            $this->getDoctrine()->getManager()->persist($media);
-            $this->getDoctrine()->getManager()->flush();
+
+            $filename = 'uploads/' . md5(uniqid()) . '.' . $media->getFile()->guessExtension();
+            $media->setPath($filename);
+            $media->getFile()->move('uploads/', basename($filename));
+
+            $em = $doctrine->getManager();
+            $em->persist($media);
+            $em->flush();
 
             return $this->redirectToRoute('admin_media_index');
         }
 
-        return $this->render('admin/media/add.html.twig', ['form' => $form->createView()]);
+        return $this->render('admin/media/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
-    /**
-     * @Route("/admin/media/delete/{id}", name="admin_media_delete")
-     */
-    public function delete(int $id)
+    #[Route('/admin/media/delete/{id}', name: 'admin_media_delete')]
+    public function delete(int $id, ManagerRegistry $doctrine): Response
     {
-        $media = $this->getDoctrine()->getRepository(Media::class)->find($id);
-        $this->getDoctrine()->getManager()->remove($media);
-        $this->getDoctrine()->getManager()->flush();
-        unlink($media->getPath());
+        $media = $doctrine->getRepository(Media::class)->find($id);
+
+        if (!$media) {
+            throw $this->createNotFoundException("Media not found");
+        }
+
+        $path = $media->getPath();
+        $em = $doctrine->getManager();
+        $em->remove($media);
+        $em->flush();
+
+        if (file_exists($path)) {
+            unlink($path);
+        }
 
         return $this->redirectToRoute('admin_media_index');
     }
